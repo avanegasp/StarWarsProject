02d9f660d335f9ab284d8e089e6f83ad8a13a165
avanegas <arco527@gmail.com>
2024-06-19 21:03:05 -0500

    Add character styles 80%

--- src/js/views/InfoDetailCharacter.jsx
+++ src/js/views/InfoDetailCharacter.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import UrlImage from "../component/Url_Image.jsx";
+import infoDetailCharacterImage from "../../img/tunel.jpg";
+import "../../styles/infoDetailCharacter.css";
 
 const InfoDetailCharacter = ({
   id,
@@ -23,38 +25,45 @@ const InfoDetailCharacter = ({
 
   return (
     <>
-      <div className="row mt-5 mb-5">
-        <div className="card col-12">
-          <div className="row">
-            <div className="col-5 mt-2 mb-4">
-              <UrlImage id={id} urlName="characters" altName="Character" />
+      <div
+        className="backgroundInfoDetailCharacter"
+        style={{
+          backgroundImage: `url(${infoDetailCharacterImage})`,
+        }}
+      >
+        <div className="row row align-items-center min-vh-100">
+          <div className="card col-12">
+            <div className="row d-flex justify-content-center">
+              <div className="picture col-5 mt-2">
+                <UrlImage id={id} urlName="characters" altName="Character" />
+              </div>
+              <div className="col-7 mt-5">
+                <h3 className="text-center">
+                  {name} {lastName}
+                </h3>
+                <p className="mt-4">{descriptionFirstParagraph}</p>
+                <p className="mt-4">{descriptionSecondParagraph}</p>
+              </div>
             </div>
-            <div className="col-7 mt-5">
-              <h3 className="text-center">
-                {name} {lastName}
-              </h3>
-              <p className="mt-4">{descriptionFirstParagraph}</p>
-              <p className="mt-4">{descriptionSecondParagraph}</p>
-            </div>
-          </div>
-          <div className="row mt-3 mb-3 ms-5">
-            <div className="col-2">
-              <p> Height: {height}</p>
-            </div>
-            <div className="col-2">
-              <p> Skin color: {skinColor}</p>
-            </div>
-            <div className="col-2">
-              <p> Birth year: {birthYear}</p>
-            </div>
-            <div className="col-2">
-              <p> Mass : {mass}</p>
-            </div>
-            <div className="col-2">
-              <p> Hair color: {hairColor}</p>
-            </div>
-            <div className="col-2">
-              <p> Eye color: {eyeColor}</p>
+            <div className="row mt-3 mb-3 ms-5">
+              <div className="col-2">
+                <p> Height: {height}</p>
+              </div>
+              <div className="col-2">
+                <p> Skin color: {skinColor}</p>
+              </div>
+              <div className="col-2">
+                <p> Birth year: {birthYear}</p>
+              </div>
+              <div className="col-2">
+                <p> Mass : {mass}</p>
+              </div>
+              <div className="col-2">
+                <p> Hair color: {hairColor}</p>
+              </div>
+              <div className="col-2">
+                <p> Eye color: {eyeColor}</p>
+              </div>
             </div>
           </div>
         </div>
