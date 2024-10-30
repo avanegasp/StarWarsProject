f980fc1585143adce84c9e93f0388be3b2ff8e94
avanegas <arco527@gmail.com>
2024-10-30 17:01:09 -0500

    Refact webpack files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,19 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const Dotenv = require('dotenv-webpack');
+const path = require("path"); // Asegúrate de importar el módulo path
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const Dotenv = require("dotenv-webpack");
+
 module.exports = merge(common, {
-    mode: 'production',
-    output: {
-        publicPath: './'
-    },
-    plugins: [
-        new Dotenv({
-            safe: true,
-            systemvars: true
-        })
-    ]
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "build"), // Especifica el directorio de salida
+    filename: "bundle.js", // Puedes especificar el nombre del archivo de salida si lo deseas
+    publicPath: "./", // Mantén esta línea para la configuración de rutas relativas
+  },
+  plugins: [
+    new Dotenv({
+      safe: true,
+      systemvars: true,
+    }),
+  ],
 });
